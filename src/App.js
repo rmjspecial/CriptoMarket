ef92b8a1da32075b44eb88a20ac152e3af86dfbc
rejoice <rmjspecial2015@gmail.com>
2022-09-06 18:52:00 +0100

    add header page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './components/Header/Header';
+import Routers from './routes/Routers';
+import './styles/App.css';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Routers />
+    </>
+  );
+}
+
+export default App;
